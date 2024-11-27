d01c5688fb2b7e928f21e726915c75ec6651b118
Emily Ng <ngem3@oocl.com>
2024-11-27 20:40:22 +0800

    feat: add updateSum feature to Counter's increment and decrement function, refactor

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
-export default function Counter() {
-    const [count, setCount] = React.useState(0);
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
+export default function Counter(props) {
+  const [count, setCount] = React.useState(0);
+  const increment = () => {
+    setCount(count + 1);
+    props.updateSum(+1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+    props.updateSum(-1);
+  };
+  React.useEffect(() => {
+    console.log("sum changed to ", props.sum);
+  }, [props.sum]);
 
-    return (
-        <div class="counter-wrapper">
-            <button onClick={increment} >+</button>
-            <span >{count}</span>
-            <button  onClick={decrement}>-</button>
-        </div>
-    );
-}
+  return (
+    <div class="counter-wrapper">
+      <button onClick={increment}>+</button>
+      <span>{count}</span>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+};
