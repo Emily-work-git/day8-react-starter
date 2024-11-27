7304a9d19328cf1ca11701df7b76c94eac9a17e3
Emily Ng <ngem3@oocl.com>
2024-11-27 20:38:39 +0800

    feat: modify CounterGroup to support display sum

--- src/component/CounterGroup.jsx
+++ src/component/CounterGroup.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import Counter from './Counter';
+import React from "react";
+import Counter from "./Counter";
 
-export default function CounterGroup(props){
-    const counterList = Array.from({length: props.size})
-        .map((data, index) => (
-            <Counter key={index + Math.random()}/>
-        ));
+export default function CounterGroup(props) {
+  const [sum, setSum] = React.useState(0);
+  const updateSum = (i) => {
+    setSum((prevSum) => prevSum + i);
+  };
+
+  const createCounterList = (size) => {
+    return Array.from({ length: size }).map((_, index) => {
+      return <Counter key={index + Math.random()} updateSum={updateSum} />;
+    });
+  };
+
+  const [counterList, setCounterList] = React.useState(createCounterList(props.size));
 
 
-    return (
-        <>
-        {counterList}
-        </>
-    )
+  React.useEffect(() => {
+    setSum(0);
+    setCounterList(createCounterList(props.size))
+  }, [props.size]);
 
+  return (
+    <>
+      <div>
+        <span>Sum: {sum}</span>
+      </div>
+      {counterList}
+    </>
+  );
 }
